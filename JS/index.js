15dc22a52f93dd7f27f14f56c7cc858ebe264c9e
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-07 16:50:55 -0300

    Possibilidade de deletar todas as tarefas

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-document.querySelector('.add-task-btn').addEventListener('click', function(){
+function addTask(){
     const text = document.querySelector('.newTask').value.trim();
     
     if(text){
@@ -6,4 +6,13 @@ document.querySelector('.add-task-btn').addEventListener('click', function(){
         newTaskElement.textContent = text;
         document.querySelector('.task-list').appendChild(newTaskElement);
     }
-});
+}
+
+function clearTask(){
+    document.querySelectorAll('li').forEach(function(task){
+        task.remove();
+    });
+}
+
+document.querySelector('.add-task-btn').addEventListener('click', addTask);
+document.querySelector('.clear-btn').addEventListener('click', clearTask);
