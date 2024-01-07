b020d161a695ee04d6c1fec47a01de42704fed0e
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-07 17:52:09 -0300

    Riscar os itens da lista

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,22 @@
+function getInputText(){
+    return document.querySelector('.newTask').value.trim();
+}
+
 function addTask(){
-    const text = document.querySelector('.newTask').value.trim();
-    
-    if(text){
+    if(getInputText()){
         const newTaskElement = document.createElement('li');
-        newTaskElement.textContent = text;
+        const newTaskCheck = document.createElement('input');
+        newTaskCheck.type = 'checkbox';
+
+        newTaskCheck.classList.add('newTaskInput');
+        newTaskElement.appendChild(newTaskCheck);
+
+        const textSpan = document.createElement('span');
+        textSpan.appendChild(document.createTextNode(getInputText()));
+        newTaskElement.appendChild(textSpan);
+    
         document.querySelector('.task-list').appendChild(newTaskElement);
+        teste(newTaskElement);
     }
 }
 
@@ -14,5 +26,12 @@ function clearTask(){
     });
 }
 
+function teste(checkbox){
+    checkbox.addEventListener('click', function() {
+        const text = checkbox.querySelector('span');
+        text.style.textDecoration = text.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+    });
+}
+
 document.querySelector('.add-task-btn').addEventListener('click', addTask);
-document.querySelector('.clear-btn').addEventListener('click', clearTask);
+document.querySelector('.clear-tasks-btn').addEventListener('click', clearTask);
