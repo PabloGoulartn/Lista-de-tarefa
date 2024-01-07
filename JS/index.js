4e23a95cac39578c2f84e172c00b936b1b0ac9a3
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-07 18:38:53 -0300

    Adição de limite de itens e mudança no CSS

--- JS/index.js
+++ JS/index.js
@@ -1,45 +1,58 @@
-function getInputText(){
-    return document.querySelector('.newTask').value.trim();
-}
+document.addEventListener('DOMContentLoaded', function(){
+    let counter = 0;
 
-function addCheckbox(newTaskElement){
-    const newTaskCheck = document.createElement('input');
-    newTaskCheck.type = 'checkbox';
-
-    newTaskCheck.classList.add('newTaskInput');
-    newTaskElement.appendChild(newTaskCheck);
-}
-
-function addSpan(newTaskElement){
-    const textSpan = document.createElement('span');
-    textSpan.appendChild(document.createTextNode(getInputText()));
-    newTaskElement.appendChild(textSpan);
-}
-
-function addTask(){
-    if(getInputText()){
-        const newTaskElement = document.createElement('li');
-        
-        addCheckbox(newTaskElement);
-        addSpan(newTaskElement);
-    
-        document.querySelector('.task-list').appendChild(newTaskElement);
-        toggleStrikethrough(newTaskElement);
+    function getInputText(){
+        return document.querySelector('.newTask').value.trim();
     }
-}
-
-function clearTask(){
-    document.querySelectorAll('li').forEach(function(task){
-        task.remove();
+    
+    function addCheckbox(newTaskElement){
+        const newTaskCheck = document.createElement('input');
+        newTaskCheck.type = 'checkbox';
+    
+        newTaskCheck.classList.add('newTaskInput');
+        newTaskElement.appendChild(newTaskCheck);
+    }
+    
+    function addSpan(newTaskElement){
+        const textSpan = document.createElement('span');
+        textSpan.appendChild(document.createTextNode(getInputText()));
+        newTaskElement.appendChild(textSpan);
+    }
+    
+    function addTask(event){
+        if(getInputText() && counter < 36){
+            counter++;
+            const newTaskElement = document.createElement('li');
+    
+            addCheckbox(newTaskElement);
+            addSpan(newTaskElement);
+        
+            document.querySelector('.task-list').appendChild(newTaskElement);
+            toggleStrikethrough(newTaskElement);
+        }
+        else{
+            alert('Limite de itens atingindo.');
+        }
+    }
+    
+    document.querySelector('#formTask').addEventListener('submit', function(event) {
+        event.preventDefault();
+        addTask();
     });
-}
-
-function toggleStrikethrough(checkbox){
-    checkbox.addEventListener('click', function() {
-        const text = checkbox.querySelector('span');
-        text.style.textDecoration = text.style.textDecoration === 'line-through' ? 'none' : 'line-through';
-    });
-}
-
-document.querySelector('.add-task-btn').addEventListener('click', addTask);
-document.querySelector('.clear-tasks-btn').addEventListener('click', clearTask);
+    
+    function clearTask(){
+        document.querySelectorAll('li').forEach(function(task){
+            task.remove();
+        });
+    }
+    
+    function toggleStrikethrough(checkbox){
+        checkbox.addEventListener('click', function() {
+            const text = checkbox.querySelector('span');
+            text.style.textDecoration = text.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+        });
+    }
+    
+    document.querySelector('.add-task-btn').addEventListener('click', addTask);
+    document.querySelector('.clear-tasks-btn').addEventListener('click', clearTask);
+});
